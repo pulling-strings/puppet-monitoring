4d019b8be822257810e4bfeac4c5ad6a890c331e
ronen <narkisr@gmail.com>
2014-09-07 23:21:23 +0300

    community repo fix and require

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,12 +15,13 @@ class monitoring(
   } ->
 
   exec{'install narkisr-sensu-plugins gems':
-    command     => "bundle install",
+    command     => 'bundle install',
     environment => ["BUNDLE_GEMFILE=${narkisr_plug_path}/Gemfile"],
     user        => 'root',
     timeout     => 600,
     path        => ['/usr/bin','/bin','/usr/local/bin'],
-    require     => [Git::Clone[$narkisr_plug_path], Package['ruby1.9.1-dev', 'build-essential']]
+    require     => [Git::Clone[$narkisr_plug_path],
+                    Package['ruby1.9.1-dev', 'build-essential']]
   }
 
   git::clone{$narkisr_plug_path:
@@ -30,7 +31,7 @@ class monitoring(
   } -> Sensu::Check<||>
 
   git::clone{$community_plug_path:
-    url   => 'git://github.com/narkisr/sensu-narkisr-plugins.git',
+    url   => 'git://github.com/sensu/sensu-community-plugins.git',
     dst   => $community_plug_path,
     owner => 'root'
   } -> Sensu::Check<||>

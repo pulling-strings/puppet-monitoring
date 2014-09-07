af3e11fe30dc4b349f2257add3abfb7b7dad69fc
ronen <narkisr@gmail.com>
2014-09-07 23:21:31 +0300

    adding serverspec monitoring

---
+++ manifests/serverspec.pp
@@ -0,0 +1,21 @@
+# Serverspec monitoring
+class monitoring::serverspec(
+  $name = '',
+  $repo ='',
+  $dst  ="/opt",
+  $specs = ''
+){
+
+  git::clone{$name:
+    url   => $repo,
+    dst   => $dst,
+    owner => 'root'
+  }
+
+  $exec = "LOCAL=true BUNDLE_GEMFILE=${::monitoring::community_plug_path}/Gemfile"
+
+  sensu::check {"${name}-sensu-serverspec":
+    command => "${exec} ${::monitoring::community_plug_path}/plugins/serverspec/check-serverspec.rb ${url} -d ${dst} -t spec/${specs}"
+  }
+  #  ./check-serverspec.rb -d /vagrant -t spec/backup/*
+}

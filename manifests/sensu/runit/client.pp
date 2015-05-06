a8da7c626345d17b499f2f65c3e2e508e33a968d
ronen <narkisr@gmail.com>
2015-05-06 23:51:09 +0300

    fixing module refernce

--- manifests/sensu/runit/client.pp
+++ manifests/sensu/runit/client.pp
@@ -7,7 +7,7 @@ class monitoring::sensu::runit::client {
   file { '/etc/service/sensu_client/run':
     ensure => file,
     mode   => 'u+x',
-    source => 'puppet:///modules/sensu_server/run_client',
+    source => 'puppet:///modules/monitoring/run_client',
     owner  => root,
     group  => root,
   }

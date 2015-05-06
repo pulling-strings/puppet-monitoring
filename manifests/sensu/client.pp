a1f5128a2d19c568ddc5980c5f81b89d62678307
ronen <narkisr@gmail.com>
2015-05-07 00:47:36 +0300

    client docker support

--- manifests/sensu/client.pp
+++ manifests/sensu/client.pp
@@ -5,9 +5,17 @@ class monitoring::sensu::client {
 
   $ruby = ['rubygems1.9.1','ruby1.9.1-dev', 'build-essential']
 
+  if($::virtual == 'docker'){
+    $manage = false
+    include monitoring::sensu::runit::client
+  } else {
+    $manage = true
+  }
+
   class { 'sensu':
     rabbitmq_vhost       => '/sensu',
     sensu_plugin_version => 'latest',
-    require              => Package[$ruby]
+    require              => Package[$ruby],
+    manage_services      => $manage,
   }
 }

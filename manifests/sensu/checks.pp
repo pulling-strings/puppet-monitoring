2a3c0fc15d116fc57ba2c8e86ce80590891bd4d8
ronen <narkisr@gmail.com>
2015-05-10 18:16:38 +0300

    folding in syncthing plugin

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -1,5 +1,8 @@
 # common checks
-class monitoring::sensu::checks {
+class monitoring::sensu::checks(
+  $token='',
+  $url='https://localhost:8080',
+){
 
   $community = "${::monitoring::sensu::plugins::base}/${::monitoring::sensu::plugins::community}"
   $custom    = "${::monitoring::sensu::plugins::base}/${::monitoring::sensu::plugins::custom}"
@@ -14,5 +17,15 @@ class monitoring::sensu::checks {
     command     => "${custom}/plugins/dropbox/check-status.rb",
     subscribers => ['dropbox'],
     standalone  => false
+  }
+
+  $exec = "BUNDLE_GEMFILE=${custom}/Gemfile /usr/local/bin/bundle exec"
+
+  sensu::check {'syncthing-status':
+    command => "${exec} ${custom}/plugins/syncthing/check-status.rb -u ${url} -t ${token}"
+  }
+
+  sensu::check {'syncthing-errors':
+    command => "${exec} ${custom}/plugins/syncthing/check-errors.rb -u ${url} -t ${token}"
   }
 }
